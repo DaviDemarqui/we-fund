000e0b3df49394d5605f5e8b8c7d45de65e99ab0
DaviDemarqui <davidemarqui@genesyssoft.com.br>
2023-12-22 17:06:37 -0300

    First commit

---
+++ we-fund/hardhat.config.js
@@ -0,0 +1,50 @@
+require("@matterlabs/hardhat-zksync-solc");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  zksolc: {
+    version: "1.3.9",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    // zksync_testnet: {
+    //   url: "https://zksync2-testnet.zksync.dev",
+    //   ethNetwork: "goerli",
+    //   chainId: 280,
+    //   zksync: true,
+    // },
+    // zksync_mainnet: {
+    //   url: "https://zksync2-mainnet.zksync.io/",
+    //   ethNetwork: "mainnet",
+    //   chainId: 324,
+    //   zksync: true,
+    // },
+  },
+  paths: {
+    artifacts: "./artifacts-zk",
+    cache: "./cache-zk",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
