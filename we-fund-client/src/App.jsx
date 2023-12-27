6f05d49b14419a4ab8a872382526c86f90702cec
DaviDemarqui <davidemarqui@genesyssoft.com.br>
2023-12-27 18:01:21 -0300

    Navbar

--- we-fund-client/src/App.jsx
+++ we-fund-client/src/App.jsx
@@ -1,20 +1,26 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import "./styles/Home.css";
-import { HomePage } from "./pages";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 export default function Home() {
   return (
-    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+    <BrowserRouter>
+      <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
         <div className="sm:flex hidden mr-10 relative">
-          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Sidebar />} />
+            {/* Define other routes and components */}
+          </Routes>
         </div>
 
         <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
           <Navbar />
+          {/* Other content/components */}
         </div>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
--- we-fund-client/src/components/Sidebar.jsx
+++ we-fund-client/src/components/Sidebar.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import { logo, sun } from '../assets';
 import { navlinks } from '../constants/navlinks';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
@@ -14,14 +16,14 @@ const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
 )
 
 const Sidebar = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const [isActive, setIsActive] = useState('dashboard');
 
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
-      {/* <Link to="/"> */}
+      <Link to="/">
         <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
-      {/* </Link> */}
+      </Link>
 
       <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
         <div className="flex flex-col justify-center items-center gap-3">
