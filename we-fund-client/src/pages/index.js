a309ab2346630332b4f39853733916d87f3d23c9
DaviDemarqui <davicoding@protonmail.com>
2023-12-27 07:28:19 -0300

    Client with tailwind and Components and pages created

---
+++ we-fund-client/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as HomePage } from './HomePage';
+export { default as ProfilePage } from './ProfilePage';
+export { default as CreateCampaign } from './CreateCampaign';
+export { default as CampaignDetails } from './CampaignDetails';
