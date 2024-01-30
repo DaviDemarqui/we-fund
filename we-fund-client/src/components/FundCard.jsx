f8f1d7e56e001369dba5e68c9a9224c6ff8b4069
Davi Demarqui <davicoding@protonmail.com>
2024-01-30 00:47:14 -0300

    New components and fixes

--- we-fund-client/src/components/FundCard.jsx
+++ we-fund-client/src/components/FundCard.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 import { tagType, thirdweb } from '../assets'
 import { daysLeft } from '../utils'
 
-function FundCard( {owner, title, description, target, deadline, amountCollected, image, handleCLick }) {
+function FundCard( {owner, title, description, target, deadline, amountCollected, image, handleClick }) {
   const remainingDays = daysLeft(deadline);
 
     return (
     <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] hover:bg-[#22222c] transition-all cursor-pointer"
-    onClick={handleCLick}>
+    onClick={handleClick}>
         <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-t-[15px]"/>
     
         <div className="flex flex-col p-4">
